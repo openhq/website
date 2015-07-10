144bd09d80b7004b72870527837e1db82d861c6e
Pete Hawkins <pete@phawk.co.uk>
2015-07-11 00:06:19 +0100

    Fix rubcop issues with puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,10 @@
 workers Integer(ENV['PUMA_WORKERS'] || 2)
-threads Integer(ENV['MIN_PUMA_THREADS']  || 8), Integer(ENV['MAX_PUMA_THREADS'] || 8)
+min_threads = Integer(ENV['MIN_PUMA_THREADS'] || 8)
+max_threads = Integer(ENV['MAX_PUMA_THREADS'] || 8)
+threads(min_threads, max_threads)
 
 preload_app!
 
-rackup      DefaultRackup
-port        ENV['PORT']     || 3000
+rackup DefaultRackup
+port ENV['PORT'] || 3000
 environment ENV['RACK_ENV'] || 'development'
