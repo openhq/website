a9ac03376f06cd69893a8cff00adcf50eae2adf3
Pete Hawkins <pete@phawk.co.uk>
2015-07-10 22:45:07 +0100

    Shorten flash output

--- spec/support/feature_helper.rb
+++ spec/support/feature_helper.rb
@@ -1,10 +1,11 @@
+require 'rack/test'
+
 module FeatureHelper
   def app
     App
   end
 end
 
-require 'rack/test'
 RSpec.configure do |config|
   config.include Rack::Test::Methods, type: :feature
   config.include FeatureHelper, type: :feature
